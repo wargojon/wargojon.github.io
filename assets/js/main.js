fdcf40a200ff2ca1dff099ebe15b37745872871d
wargojon <wargojon@msu.edu>
2016-02-21 20:36:24 -0500

    map location update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 jQuery(document).ready(function($) {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([42.7348, 84.4808], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.mapbox-streets-v6',
     accessToken: 'pk.eyJ1Ijoid2FyZ29qb24iLCJhIjoiY2lreGFsaDRiMDB0bXVha3NhMTc2ZWhieCJ9.BycVdXgYJA7g5XueVh9D3A'
     }).addTo(map);
-    alert(map);
     /* ======= Scrollspy ======= */
     $('body').scrollspy({ target: '#header', offset: 400});
     
